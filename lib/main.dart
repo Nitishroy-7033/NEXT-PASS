b032e14bfa929ae1c578b97352cd2848513ee464
HarshKumarGupta88 <harshgupta88125@gmail.com>
2025-01-30 21:30:05 +0530

    home screen updated 2

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'NEXT PASS',
-      initialRoute: AppRoutes.home,//AppRoutes.allWidgetsScreen,
+      initialRoute: AppRoutes.allWidgetsScreen,
       getPages: RouteGenerator.getRoutes(),
       initialBinding: SplashBinding(),
       theme: AppThemes.lightTheme,
