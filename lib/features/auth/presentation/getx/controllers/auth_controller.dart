5c0c5dccbdc21a55ebf60e0bd989fcc81f48ec61
Waqas Muhammad <waqas.developer5254@gmail.com>
2025-02-01 12:55:43 +0500

    navbar

--- lib/features/auth/presentation/getx/controllers/auth_controller.dart
+++ lib/features/auth/presentation/getx/controllers/auth_controller.dart
@@ -1,17 +1,11 @@
 import 'package:get/get.dart';
 
 import '../../../domain/entities/user.dart';
-import '../../../domain/usecases/user_login.dart';
-import '../../../domain/usecases/user_signup.dart';
-
 
 class AuthController extends GetxController {
- 
-
   var email = ''.obs;
   var password = ''.obs;
   var name = ''.obs;
   var isLoading = false.obs;
   var user = Rxn<User>();
-
 }
