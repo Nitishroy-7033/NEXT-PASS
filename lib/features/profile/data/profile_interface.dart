6b3308251c2ad6453fc7c5e0f41881011a299862
rahul <rb6764385@gmail.com>
2025-04-13 03:06:43 +0530

    profile details fetch and showing the profile section

--- lib/features/profile/data/profile_interface.dart
+++ lib/features/profile/data/profile_interface.dart
@@ -2,4 +2,5 @@ import '../../../core/constants/app_linker.dart';
 
 abstract interface class ProfileInterface {
   Future<ApiResponse<ProfileModel>> GetProfileInfo();
+  // Profile information fetch by API Calling
 }
